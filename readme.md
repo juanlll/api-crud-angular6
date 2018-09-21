09602d610c8d5beb3e53ffcec76cb176ac2d61a1
juan vargas <juanvargasva.seo@gmail.com>
2018-09-21 10:22:09 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+<h1 syle="color:red;">PROYECTO JWT CON LARAVEL 5.5</h1>
+
+<h4>PASOS PARA INSTALAR</h4>
+<ul>
+<li>composer install</li>	
+<li>php artisan key:generate</li>
+<li>php artisan jwt:generate</li>
+<li>php artisan serve</li>
+</ul>
+
+
+
+
+<h4>LICENCIA MIT</h4>
+
+
+
+
---
+++ routes/api.php
@@ -0,0 +1,103 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+
+
+
+Route::get('record/{id}',function($id){
+	$record = App\Record::findOrFail($id);
+	return json_encode(["record"=>$record,"message"=>"Record consultado con id = {$id}"]);
+});
+
+
+Route::post('rec',function(Request $request){
+
+		$rules =[
+	    'temp' => 'required|digits_between:1,5|numeric',
+	    'humidity' => 'required|digits_between:1,5|numeric',
+	    'co2' => 'required|digits_between:1,5|numeric',
+		];
+
+       $validator = \Validator::make($request->all(),$rules);
+
+		if ($validator->fails()) {
+		   return response()->json($validator->errors(), 422);
+		}
+
+	$record = new App\Record;
+	$record->temp = $request->input('temp');
+	$record->humidity = $request->input('humidity');
+	$record->co2 = $request->input('co2');
+	$record->save();
+	return json_encode(["record"=>$record,"message"=>"Registrado correctamente!"]);
+});
+
+
+
+
+Route::get('records',function(){
+	$records = App\Record::orderBy('id', 'ASC')->get();
+	return json_encode($records);
+});
+
+
+
+
+
+Route::put('record/{id}',function(Request $request,$id){
+
+	$rules =[
+	    'temp' => 'required|digits_between:1,5|numeric',
+	    'humidity' => 'required|digits_between:1,5|numeric',
+	    'co2' => 'required|digits_between:1,5|numeric',
+		];
+
+       $validator = \Validator::make($request->all(),$rules);
+
+		if ($validator->fails()) {
+		   return response()->json($validator->errors(), 422);
+		}
+
+
+	$record = App\Record::findOrFail($id);
+	$record->temp = $request->input('temp');
+	$record->humidity = $request->input('humidity');
+	$record->co2 = $request->input('co2');
+	$record->update();
+	return json_encode($record);
+});
+
+Route::delete('record/{id}',function($id){
+	$record = App\Record::findOrFail($id);
+	$record->delete();
+	return json_encode(['message'=>"Tu record fue eliminado!"]);
+});
+
+
+
+Route::middleware(['jwt.auth'])->group(function(){
+	Route::get('users','UserController@index');
+
+});
+
+
+Route::post('login','AuthenticateController@authenticate');
